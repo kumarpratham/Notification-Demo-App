b8fea62a2d2accc21626184f7bd4cdedf23c9069
Pratham Kumar <150330048+kumarpratham@users.noreply.github.com>
2024-01-09 02:26:31 +0530

    Second commit ( Done )

--- app/src/main/java/com/coding/notificationpushdemo/MainActivity.kt
+++ app/src/main/java/com/coding/notificationpushdemo/MainActivity.kt
@@ -2,10 +2,20 @@ package com.coding.notificationpushdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.coding.notificationpushdemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+
+        binding.btn.setOnClickListener {
+            Toast.makeText(this,"The Notification Pushed successfully!",Toast.LENGTH_LONG).show()
+
+        }
     }
 }
